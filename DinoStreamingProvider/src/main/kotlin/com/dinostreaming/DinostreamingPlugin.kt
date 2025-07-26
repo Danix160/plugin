96e167bf4d8d63edf9abcc2cbd20b731cbe9cb2c
Danix160 <tiziotipo18@gmail.com>
2025-07-27 01:28:18 +0200

    Update DinostreamingPlugin.kt

--- DinoStreamingProvider/src/main/kotlin/com/dinostreaming/DinostreamingPlugin.kt
+++ DinoStreamingProvider/src/main/kotlin/com/dinostreaming/DinostreamingPlugin.kt
@@ -1,25 +1,10 @@
 package com.dinostreaming
 
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
-import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
+import com.lagradost.cloudstream3.plugins.PluginContext
 
-@CloudstreamPlugin
-class DinostreamingPlugin: Plugin() {
-    private var activity: AppCompatActivity? = null
-
-    override fun load(context: Context) {
-        activity = context as? AppCompatActivity
-
-        // All providers should be added in this manner
-        registerMainAPI(DinoStreamingProvider())
-
-        openSettings = {
-            val frag = BlankFragment(this)
-            activity?.let {
-                frag.show(it.supportFragmentManager, "Frag")
-            }
-        }
+class DinostreamingPlugin : Plugin() {
+    override fun load(context: PluginContext) {
+        registerMainAPI(DinoStreaming())
     }
 }
