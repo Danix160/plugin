7619b47ed067c82e3c9c4efa99065a2f5ac2e8c5
Danix160 <tiziotipo18@gmail.com>
2025-07-27 01:38:39 +0200

    Update DinostreamingPlugin.kt

--- DinoStreamingProvider/src/main/kotlin/com/dinostreaming/DinostreamingPlugin.kt
+++ DinoStreamingProvider/src/main/kotlin/com/dinostreaming/DinostreamingPlugin.kt
@@ -1,6 +1,7 @@
 package com.dinostreaming
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.registerMainAPI // <-- Import mancante!
 
 @CloudstreamPlugin
 class DinoStreamingPlugin {
