4dd56028cd19ca91e6829a84dce1fd623b05f059
Danix160 <tiziotipo18@gmail.com>
2025-07-27 00:53:31 +0200

    Update and rename x.txt to build.gradle.kts

---
+++ DinoStreamingProvider/build.gradle.kts
@@ -0,0 +1,22 @@
+dependencies {
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+}
+version = 1 // ✅ questa è fuori dal blocco cloudstream
+
+cloudstream {
+    language = "it"
+    description = "Movies and Shows from DinoStreaming"
+    authors = listOf("doGior")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    requiresResources = false
+    iconUrl = "https://dinostreaming.it/favicon.ico"
+}
+
+android {
+    buildFeatures {
+        buildConfig = true
+        viewBinding = true
+    }
+}
