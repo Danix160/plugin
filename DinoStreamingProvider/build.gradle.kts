9e8722ad17e7f4518c50a44706e928bbe396e44e
Danix160 <tiziotipo18@gmail.com>
2025-07-27 02:23:16 +0200

    Update build.gradle.kts

--- DinoStreamingProvider/build.gradle.kts
+++ DinoStreamingProvider/build.gradle.kts
@@ -1,25 +1,23 @@
-dependencies {
-    dependencies {
-    implementation("androidx.databinding:viewbinding:8.2.0") // o compatibile con AGP in uso
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-    implementation("com.github.recloudstream.cloudstream:library:v4.5.2")
+plugins {
+    id("cloudstream-plugin") version "0.0.1" // questo è un plugin custom, definito in `pluginManagement` o `buildscript`
+    kotlin("jvm") version "1.9.10"
 }
-version = 1 // ✅ questa è fuori dal blocco cloudstream
 
-cloudstream {
+cloudstreamPlugin {
     language = "it"
-    description = "Movies and Shows from DinoStreaming"
+    status: 1
     authors = listOf("doGior")
-    status = 1
     tvTypes = listOf("Movie", "TvSeries")
     requiresResources = false
     iconUrl = "https://dinostreaming.it/favicon.ico"
 }
 
-android {
-    buildFeatures {
-        buildConfig = true
-        viewBinding = true
-    }
+dependencies {
+     implementation("com.github.recloudstream.cloudstream:library:v4.5.2")
+}
+
+repositories {
+    google()
+    mavenCentral()
+    maven("https://jitpack.io")
 }
